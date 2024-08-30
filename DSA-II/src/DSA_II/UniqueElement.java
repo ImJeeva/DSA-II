264e3e53d357b7fd3fe3834f32a00b49ec2b19cf
ImJeeva <romanjeeva22@gmail.com>
2024-08-30 22:05:03 +0530

    DSA

---
+++ DSA-II/src/DSA_II/UniqueElement.java
@@ -0,0 +1,41 @@
+package DSA_II;
+
+
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+// Question: Find the lonely integer in an array that value not have a pair.
+
+
+public class UniqueElement {
+	static void UElement(int[] arr) {
+
+		HashSet<Integer> h = new HashSet<Integer>();
+		for (int i : arr) {
+			if (h.contains(i)) {
+				h.remove(i); // If number already present that will remove from HashSet.
+			} else {
+				h.add(i); 
+			}
+
+		}
+		for (int x : h) {
+			System.out.println(x);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] arr = new int[n];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+		UElement(arr);
+
+	}
+
+}
